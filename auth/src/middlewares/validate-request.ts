fc1cc61cc9aef3ae1781c627dd6925a442d38d21
sasha mihalache <sasha.mihalache@gmail.com>
2020-05-26 22:49:04 +0300

    add testing infra for auth service and write first test

--- auth/src/middlewares/validate-request.ts
+++ auth/src/middlewares/validate-request.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Next, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
 
