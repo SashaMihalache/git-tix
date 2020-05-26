d3c6d3c7c741fbf7fcb58c9099aa1e67808dac83
sasha mihalache <sasha.mihalache@gmail.com>
2020-05-26 23:31:29 +0300

    add tests for all routes

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -2,11 +2,54 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("returns a 201 on successful signup", async () => {
-  return request(app)
+  await request(app)
     .post("/api/users/signup")
     .send({
       email: "test@test.com",
       password: "password",
     })
     .expect(201);
+});
+
+it("returns a 400 with an invalid email", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "p",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with missing email and password", async () => {
+  await request(app).post("/api/users/signup").send({}).expect(400);
+});
+
+it("dissalows duplicate emails", async () => {
+  const user = { email: "test@test.com", password: "password" };
+
+  await request(app).post("/api/users/signup").send(user).expect(201);
+  await request(app).post("/api/users/signup").send(user).expect(400);
+});
+
+it("sets a cookie after a successful signup", async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
 });
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -24,7 +24,6 @@ router.post(
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      console.log("Email in use");
       throw new BadRequestError("Email in use");
     }
 
