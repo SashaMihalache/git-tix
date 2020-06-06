82ee3b4ec81ac85bf67634e28f955a2ed2ebf134
sasha mihalache <sasha.mihalache@gmail.com>
2020-06-06 14:01:50 +0300

    add generics for events through TS

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,12 +1,15 @@
 import { Message } from "node-nats-streaming";
 import { Listener } from "./base-listener";
-
-export class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  // subject: Subjects.TicketCreated = Subjects.TicketCreated; same as below
+  readonly subject = Subjects.TicketCreated;
   queueGroupName = "payments-service";
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message) {
     console.log("Event data!", data);
+    console.log(data.price);
 
     msg.ack();
   }
